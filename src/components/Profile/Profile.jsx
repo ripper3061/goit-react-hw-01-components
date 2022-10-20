afb037bc87e3e01b2cabca441ed4ba3f33ae4b55
Vladimir Ponomarov <ripper3061@gmail.com>
2022-10-20 11:47:08 +0300

    task 1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,40 @@
+import {
+  ProfileCard,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatsItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
+
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt={username} class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
-      </div>
+    <ProfileCard>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </ProfileCard>
   );
 };
--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,1 +1,62 @@
 import styled from '@emotion/styled';
+
+export const ProfileCard = styled.div`
+  margin: 25px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 320px;
+  box-shadow: 0px 1px 5px 3px rgba(135, 132, 132, 0.98);
+  -webkit-box-shadow: 0px 1px 5px 3px rgba(135, 132, 132, 0.98);
+  -moz-box-shadow: 0px 1px 5px 3px rgba(135, 132, 132, 0.98);
+`;
+
+export const Description = styled.div`
+  padding: 30px;
+  text-align: center;
+`;
+
+export const Avatar = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: antiquewhite;
+`;
+
+export const Name = styled.p`
+  font-weight: 700;
+`;
+
+export const Tag = styled.p`
+  color: #767676;
+`;
+
+export const Location = styled.p`
+  color: #767676;
+`;
+
+export const Stats = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+  justify-content: space-evenly;
+  list-style-type: none;
+`;
+
+export const StatsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  text-align: center;
+  border: 1px solid #d7d7d7;
+  width: 33.33%;
+  background-color: #f4f4f4;
+`;
+
+export const Label = styled.span`
+  font-size: 14px;
+  color: #9a9a9a;
+`;
+
+export const Quantity = styled.span`
+  font-weight: 700;
+`;
