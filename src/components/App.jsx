fb9147ab21fff704dbd783b43a7e6d935ecaac9c
Vladimir Ponomarov <ripper3061@gmail.com>
2022-10-20 10:39:23 +0300

    task1 - рендер карточки + подключил emotion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,13 @@ import statistics from './data/data.json';
 export const App = () => {
   return (
     <div>
-      <Profile user={user} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics data={statistics} />
     </div>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,25 @@
-export const Profile = ({
-  user: { username, tag, location, avatar, stats },
-}) => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div>
-      <div>
-        <img src={avatar} alt={username} />
-        <p>{username}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
       </div>
 
-      <ul>
+      <ul class="stats">
         <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
         </li>
         <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
