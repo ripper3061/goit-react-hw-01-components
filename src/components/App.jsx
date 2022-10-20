eee17ed4b44f9be8cd7091dc1f00f7e60e8dbf31
Vladimir Ponomarov <ripper3061@gmail.com>
2022-10-20 10:26:37 +0300

    какой-то прогресс

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Profile } from './Profile/Profile';
+import user from './data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import statistics from './data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile user={user} />
+      <Statistics data={statistics} />
     </div>
   );
 };
